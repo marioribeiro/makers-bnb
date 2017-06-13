99ad00c61ef31878ad352781775000d24a45c362
y0m0 <lixi.daniele@gmail.com>
2017-06-13 18:01:41 +0100

    Adds a sligthly better way to run the tests

--- test/spaceTest.js
+++ test/spaceTest.js
@@ -1,12 +1,17 @@
-var mongoose = require('mongoose');
-var expect = require('expect.js');
-var assert = require('assert');
+var mongoose = require('mongoose'),
+    chai = require('chai'),
+    assert = chai.assert,
+    expect = chai.expect;
+// sligthly changed the package requirement
+// instead of import should and expect we can import chai
+// chai has all of them: assert, expect, should
+
+
 mongoose.connect('mongodb://localhost/makersbnb_test');
 
 describe('Space', function() {
 
   var Space = require('../models/space');
-  var space;
 
   beforeEach(function(done){
     Space.remove({}, function(){
@@ -23,5 +28,22 @@ describe('Space', function() {
       });
     });
   });
+
+// this is just a maybe better version of the previous test, it implements the use of the .then keyword
+// that is part of Mocha
+  it("Check that a Space is being added", function(done) {
+    var space = new Space({name: 'test2'});
+    space.save()
+    // we can use the .then keyword to avoid nesting the rest of the test inside the save()
+    // the next lines are going to be executed only after save() is done
+      .then(function() {
+        Space.find({}, function(err, spaces) {
+          expect(spaces[0].name).to.equal("test2");
+          done();
+        });
+      });
+  });
+
+
 
 });
