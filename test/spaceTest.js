295fc08fe30996df26305f2d71737bc859746d66
“rogrenke” <roland@rolandgrenke.com>
2017-06-13 17:13:03 +0100

    Refactors tests, adds users route

--- test/spaceTest.js
+++ test/spaceTest.js
@@ -9,19 +9,18 @@ describe('Space', function() {
   var space;
 
   beforeEach(function(done){
-    Space.remove({}, function(err){});
-    var space = new Space({name: 'test'});
-    space.save(function(err){
+    Space.remove({}, function(){
       done();
     });
-
   });
 
   it("Check that a Space is being added to the database", function(done) {
-
-    Space.find({}, function(err, spaces) {
-      expect(spaces.pop().name).to.equal("test");
-      done();
+    var space = new Space({name: 'test'});
+    space.save(function(){
+      Space.find({}, function(err, spaces) {
+        expect(spaces[0].name).to.equal("test");
+        done();
+      });
     });
   });
 
