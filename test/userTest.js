5472a59979f5fae83d7dc136159628d07f816e9f
Kynosaur <kye.bracey@gmail.com>
2017-06-14 10:39:51 +0100

    Add tests for user creation and validation, add user model with required fields of name and email, and password field

--- test/userTest.js
+++ test/userTest.js
@@ -1,28 +1,42 @@
 var mongoose = require('mongoose');
-var expect = require('expect.js');
+var chai = require('chai'),
+    assert = chai.assert,
+    expect = chai.expect;
+
+mongoose.Promise = global.Promise;
+
 mongoose.connect('mongodb://localhost/makersbnb_test');
-
-var User = require('../models/user');
-
-beforeEach(function(done){
-    User.remove({}, function(err){});
-    var testUserDefault = new User({ name: 'Testings', email: "test_default@test.com", password: 'test_password'});
-    testUserDefault.save(function(err){
-      done()
-    })
-  });
 
 describe('User', function() {
 
-  it("creates user", function(done){
-    var testUser = new User({ name: 'Testings', email: "test@test.com", password: 'test_password'});
-    testUser.save();
-    User.find({"email": "test@test.com"}, function(err, users) {
-      console.log(users);
-      expect(users[0].email).to.equal("test@test.com");
+  var User = require('../models/user')
+
+  beforeEach(function(done) {
+    mongoose.connection.collections.users.drop(function() {
       done();
     });
-    
   });
 
-})
+  describe('User creation', function() {
+    it("creates user", function(done){
+      var testUser = new User({ name: 'Testing', email: "test@test.com", password: 'test_password'});
+      testUser.save(function() {
+        User.find({"email": "test@test.com"}, function(err, users) {
+          expect(users[0].name).to.equal("Testing");
+          done();
+        });
+      });
+    });
+  });
+
+
+  describe('User validation', function() {
+    it('Does not create a user if an email is not passed', function(done) {
+      var testUser = new User({name: 'testing', password: 'test_password'});
+      testUser.validate(function(err) {
+        expect(err.errors.email).to.exist;
+        done();
+      });
+    });
+  });
+});
