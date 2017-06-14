1143d3c11e5625cf133748f4ab61ce9e69f49455
Mário Ribeiro <mail@marioribeiro.com>
2017-06-14 11:56:19 +0100

    Add signup form template
    - Add feature tests

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('signup', { title: 'Sign Up' });
 });
 
 module.exports = router;
---
+++ views/test/userFeatureTest.js
@@ -0,0 +1,25 @@
+process.env.NODE_ENV = 'test';
+var server = require('../app');
+var http = require('http');
+var assert = require('assert');
+
+var Browser = require('zombie');
+
+describe('user signup page', function(){
+  before(function(){
+    this.server = http.createServer(server).listen(3000)
+    this.browser = new Browser({ site: 'http://localhost:3000/users' });
+  });
+
+  it('should show a signup form', function(){
+    assert.ok(this.browser.success);
+    assert.equal(this.browser.text('h1'), 'Sign Up');
+    assert.equal(this.browser.text('form label'), 'NameEmailPassword');
+  });
+  it('should refuse empty submissions');
+
+  after(function(done){
+    this.server.close(done);
+  });
+  
+});
