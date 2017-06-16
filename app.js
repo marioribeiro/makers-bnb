21f9644f89c3b1c8df191a2e397f6b27a1dac8a3
Mário Ribeiro <mail@marioribeiro.com>
2017-06-16 10:08:04 +0100

    Improve mongoose setup
    - Refactor

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 var express = require('express');
+var session = require('express-session');
+var mongoose = require('mongoose');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
-var session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 var spaces = require('./routes/spaces');
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// mongoose setup
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/makersbnb_dev');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
